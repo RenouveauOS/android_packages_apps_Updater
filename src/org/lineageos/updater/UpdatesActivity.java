d9bc4165fafb3cb3141912d4dcdd61df072498c1
JoshuaDoes <joshuadoespersonalstuff@gmail.com>
2019-08-12 11:08:51 -0400

    Create TextView headerLastCheck before setting situational text
    Change-Id: Id92c04d22b58b52cde17777ce2df095d95d63c6f

--- src/org/lineageos/updater/UpdatesActivity.java
+++ src/org/lineageos/updater/UpdatesActivity.java
@@ -364,13 +364,13 @@ public class UpdatesActivity extends UpdatesListActivity {
         final SharedPreferences preferences =
                 PreferenceManager.getDefaultSharedPreferences(this);
         long lastCheck = preferences.getLong(Constants.PREF_LAST_UPDATE_CHECK, -1) / 1000;
+        TextView headerLastCheck = (TextView) findViewById(R.id.header_last_check);
         if (lastCheck == 0) {
             headerLastCheck.setText(R.string.header_last_updates_check_never);
         } else {
             String lastCheckString = getString(R.string.header_last_updates_check,
                     StringGenerator.getDateLocalized(this, DateFormat.LONG, lastCheck),
                     StringGenerator.getTimeLocalized(this, lastCheck));
-            TextView headerLastCheck = (TextView) findViewById(R.id.header_last_check);
             headerLastCheck.setText(lastCheckString);
         }
     }
